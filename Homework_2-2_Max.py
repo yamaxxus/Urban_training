4ea8bad981de23af44b591118e97a3bd16f67209
yamaxxus <maksipol@gmail.com>
2024-09-28 13:05:29 +0300

    Add files via upload
    Условная конструкция. Операторы if, elif, else.

---
+++ Homework_2-2_Max.py
@@ -0,0 +1,29 @@
+first = 2
+second = 2
+third = 2
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+
+first = 88
+second = 15
+third = 88
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
+
+first = 3
+second = 4
+third = 5
+if first == second == third:
+    print(3)
+elif first == second or second == third or first == third:
+    print(2)
+else:
+    print(0)
